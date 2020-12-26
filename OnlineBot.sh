ae27a8f1795a536167cdfd59ef4b64dcce823f31
Gr8 Username <jmrjmrlabs@risley.net>
2020-12-26 04:48:46 +0000

    Add new feature, the ability to get notifications for multiple players!!!

--- OnlineBot.sh
+++ OnlineBot.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
-uuid='UUID-OF-PLAYER'
-# the uuid of the player, this setting is ignored if userUsername=true.
-playerName='player'
-# if userUsername=true, please fill in this option, and ignore uuid=
+playerName[0]='player'
+# playerName[1]='player 2'
+# specify the names of the players you wish to recieve notifications for, to add multiple players
+# add a line with playerName[2]='PLAYER2', playerName[3]='PLAYER3' etc.
 key='HYPIXEL-API-KEY-GOES-HERE'
+# hypixel API key
 TGbotAPIkey='TELEGRAM-API-KEY-GOES-HERE'
-TGbotChannel='CHANNEL-NUMBER-OF-TELEGRAM-CHAT'
-# fill in these options
-useUsername=true
-# The above option determines whether the script will get the uuid based upon the username of the "playerName" variable,
-# this will save you the trouble of filling in a uuid, but might also cause problems if a player changes their name.
-if [ $useUsername = true ]
-then
-	# gets the uuid of the player from mojang's API.
-	uuid=$( curl https://api.mojang.com/users/profiles/minecraft/"$playerName"?at=0 | grep -o "\"id\":\"[a-z0-9]*\"" | grep -o "\"[a-z0-9]*\"$" | sed -s 's/"//g' )
-	echo uuid is "$uuid"
-fi
+# Telegram API key
+TGbotChannel='TELEGRAM-CHAT-CHANNEL-NUMBER'
+# Telegram chat channel
+for index in ${!playerName[*]}
+do
+	echo player is ${playerName[$index]}
+	playerUUID[$index]=$( curl https://api.mojang.com/users/profiles/minecraft/${playerName[$index]}?at=0 | grep -o "\"id\":\"[a-z0-9]*\"" | grep -o "\"[a-z0-9]*\"$" | sed -s 's/"//g' )
+	echo uuid for "${playerName[$index]}" is "${playerUUID[$index]}"
+done
 while true
 do
-	runCheck=$( curl https://api.hypixel.net/status?key="$key"\&uuid="$uuid" )
-	echo "$runCheck"
-	isOnline=$( echo "$runCheck" | grep -o "\"online\":true")
-	if [ "$isOnline" != "" ]
-	then
-		if [ "$playerStatus" != 'online' ]
+	for index in ${!playerName[*]}
+	do
+		runCheck=$( curl https://api.hypixel.net/status?key="$key"\&uuid="${playerUUID[$index]}" )
+		echo "$runCheck"
+		isOnline=$( echo "$runCheck" | grep -o "\"online\":true")
+		if [ "$isOnline" != "" ]
 		then
-			tgMSG=$( echo "$playerName" has joined )
-			playerStatus=online
-			curl https://api.telegram.org/bot"$TGbotAPIkey"/sendMessage?chat_id="$TGbotChannel"\&text="$tgMSG"
+			if [ "${playerStatus[$index]}" != 'online' ]
+			then
+				tgMSG=$( echo "${playerName[$index]}" has joined )
+				playerStatus[$index]=online
+				curl https://api.telegram.org/bot"$TGbotAPIkey"/sendMessage?chat_id="$TGbotChannel"\&text="$tgMSG"
+			fi
+			#playerStatus=offline
+		else
+			if [ "${playerStatus[$index]}" != 'offline' ]
+			then
+				playerStatus[$index]=offline
+				tgMSG=$( echo "${playerName[$index]}" has left )
+				curl https://api.telegram.org/bot"$TGbotAPIkey"/sendMessage?chat_id="$TGbotChannel"\&text="$tgMSG"
+			fi
 		fi
-		#playerStatus=offline
-	else
-		if [ "$playerStatus" != 'offline' ]
-		then
-			playerStatus=offline
-			tgMSG=$( echo "$playerName" has left )
-			curl https://api.telegram.org/bot"$TGbotAPIkey"/sendMessage?chat_id="$TGbotChannel"\&text="$tgMSG"
-		fi
-	fi
-	sleep 32
+		sleep 10
+	done
 done
